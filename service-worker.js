9caa44cb3fb2e74ccf6304a825a796b594525a6e
pgb2409 <bateriapartituras@gmail.com>
2025-10-03 20:36:21 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('bateria-cache').then(function (cache) {
+      return cache.addAll([
+        'index.html',
+        'style.css',
+        'script.js',
+        'manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
